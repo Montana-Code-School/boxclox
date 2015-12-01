f3addc6453a07cb170fae627e5828e4225876ccf
sreahard <suziequz@msn.com>
2015-12-01 15:01:31 -0700

    The right initial design commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'BoxClox - A roller derby penalty box clocking app' });
 });
 
 module.exports = router;
