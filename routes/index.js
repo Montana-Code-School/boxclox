6f28642774ea4693a06c6d938e9a3676e0ad9ab6
sreahard <suziequz@msn.com>
2015-12-01 13:44:10 -0700

    index.js changed title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('./views/error.ejs', { title: 'BoxClox - A roller derby penalty timer app' });
 });
 
 module.exports = router;
