e812fb6211d192c99d5fc969691a2257b04d27a5
Juniper Chapman <junipie@gmail.com>
2015-12-01 15:52:27 -0700

    Commiting for pull

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('./views/error.ejs', { title: 'BoxClox - A roller derby penalty timer app' });
 });
 
+router.get('/timer', function(req, res, next){
+	res.render('./timer.ejs');
+});
+
 module.exports = router;
