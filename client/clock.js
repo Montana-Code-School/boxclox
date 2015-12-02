05511b4756fd741e8c5dcba9db134569decc951c
Juniper Chapman <junipie@gmail.com>
2015-12-01 17:03:46 -0700

    Trying to fix react issue

--- client/clock.js
+++ client/clock.js
@@ -1,4 +1,5 @@
-React = require('react');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
 
 var App = React.createClass({
@@ -11,4 +12,4 @@ var App = React.createClass({
    }
 });
 
-React.render(<App/>, document.getElementById("timer-here"));
+ReactDOM.render(<App/>, document.getElementById("timer-here"));
