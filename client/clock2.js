a3f170382fd750846c48a56806b3141f9490d19c
Juniper Chapman <junipie@gmail.com>
2015-12-03 11:02:47 -0700

    Lint continue

--- client/clock2.js
+++ client/clock2.js
@@ -1,33 +1,33 @@
 var React = require('react');
 var CircularProgress = require('material-ui').CircularProgress;
 var Clock = React.createClass({
-    getDefaultProps: function() {
-        return {
-            time: 25 * 60 * 1000,
-            maxtime: 25 * 60 * 1000,
-        };
-    },
+  getDefaultProps: function() {
+    return {
+      time: 25 * 60 * 1000,
+      maxtime: 25 * 60 * 1000,
+  };
+},
 
-    getTime: function() {
-        var now = new Date(this.props.time).toUTCString();
-        return now.slice(23,-4);
-    },
+getTime: function() {
+    var now = new Date(this.props.time).toUTCString();
+    return now.slice(23,-4);
+},
 
-    getPercent: function() {
-        return 100 - ((this.props.maxtime - this.props.time) / this.props.maxtime * 100);
-    },
+getPercent: function() {
+    return 100 - ((this.props.maxtime - this.props.time) / this.props.maxtime * 100);
+},
 
-    render: function() {
-        
-        return (
-            <div className="clock">
-               {this.getTime()}
-                <div className="circular">
-                    <CircularProgress mode="determinate" color="black" value={this.getPercent()} size={2} />
-                </div>
-            </div>
+render: function() {
+
+    return (
+        <div className="clock">
+        {this.getTime()}
+        <div className="circular">
+        <CircularProgress mode="determinate" color="black" value={this.getPercent()} size={2} />
+        </div>
+        </div>
         );
-    }
+}
 });
 
 module.exports = Clock;
