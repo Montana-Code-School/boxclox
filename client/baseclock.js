3b1156d0417ee2ba373f04d4aa72c61881fac5ae
Juniper Chapman <junipie@gmail.com>
2015-12-03 10:41:52 -0700

    Lint

--- client/baseclock.js
+++ client/baseclock.js
@@ -3,19 +3,17 @@ var sec = 0;
 
 function startClock() {
   if (clicked === false) {
-    clock = setInterval("stopWatch()", 1000);
+    var clock = setInterval(stopWatch(), 1000);
     clicked = true;
-  }
-  else if (clicked === true) {
   }
 }
 function stopWatch() {
   sec++;
-  document.getElementById("timer").innerHTML = sec;
+  document.getElementById('timer').innerHTML = sec;
 }
 function stopClock() {
   window.clearInterval(clock);
   sec = 0;
-  document.getElementById("timer").innerHTML=0;
+  document.getElementById('timer').innerHTML = 0;
   clicked = false;
 }
