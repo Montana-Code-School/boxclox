bcbb14f5b1fc71cd71ab1450d900ded0b4681039
Juniper Chapman <junipie@gmail.com>
2015-12-03 15:26:29 -0700

    Production needs?

--- client/oneTeam.js
+++ client/oneTeam.js
@@ -5,24 +5,28 @@ var Colors = require('material-ui/lib/styles/colors');
 
 var OneClock = require('./oneClock');
 
+var ChooseColor = require('./colorPicker');
+
 var OneTeam = React.createClass({
   render: function() {
         return (
             <div>
-            <div className="clock-button"  style={{backgroundColor: 'red'}}>
+              <div>
+                <ChooseColor/>
+              </div>
+              <div className='clock-button'  style={{backgroundColor: 'red'}}>
+                <OneClock/>
+              </div>
+              <div className='clock-button'  style={{backgroundColor: 'blue'}}>
               <OneClock/>
               </div>
-            <div className="clock-button"  style={{backgroundColor: 'blue'}}>
-            <OneClock/>
-            </div>
-            <div className="clock-button"  style={{backgroundColor: 'green'}}>              
-            <OneClock/>
-            </div>
+              <div className='clock-button'  style={{backgroundColor: 'green'}}>              
+              <OneClock/>
+              </div>
             </div>
         );
     }
 });
 
 
-ReactDOM.render(<OneTeam/>, document.getElementById('timer-one'));
-ReactDOM.render(<OneTeam/>, document.getElementById('timer-two'));
+module.exports = OneTeam;
