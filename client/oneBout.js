5daddc5333f56368bf1e03c24ac2c4f4f2b8d5fa
sreahard <suziequz@msn.com>
2015-12-15 12:22:01 -0700

    linting all the things

--- client/oneBout.js
+++ client/oneBout.js
@@ -20,38 +20,37 @@ var Bout = React.createClass({
       clockOneStarted: false
     };
   },
-  handleClockClick: function(whichClock){
-    var newState = {};
-    newState[whichClock + 'Started'] = !this.state[whichClock + 'Started'];
-    this.setState(newState);
-    if (this.state.pause === true){
-      alert('The jam is off, this timer will start onced you click the JAM START button.')
-    }
-
-  },
-  handleResetClick: function(whichClock){
-    var newState = {};
-    newState[whichClock] = 300000;
-    newState[whichClock + 'Started'] = false;
-    this.setState(newState);
-  },
-  clockTickCallback: function(whichClock){
-    if(!this.state.pause){
-      var newState = {};
-      newState[whichClock] = this.state[whichClock] - 1000;
-      this.setState(newState);
-    } 
-    if (this.state[whichClock] <= 0) {
-      this.handleResetClick(whichClock);
-    }
-  },
   getPauseInfo: function() {
     if (this.state.pause) {
       return 'JAM START';
     } else {
       return 'JAM STOP';
     }
-  }, 
+  },
+  clockTickCallback: function(whichClock) {
+    if (!this.state.pause) {
+      var newState = {};
+      newState[whichClock] = this.state[whichClock] - 1000;
+      this.setState(newState);
+    }
+    if (this.state[whichClock] <= 0) {
+      this.handleResetClick(whichClock);
+    }
+  },
+  handleResetClick: function(whichClock) {
+    var newState = {};
+    newState[whichClock] = 300000;
+    newState[whichClock + 'Started'] = false;
+    this.setState(newState);
+  },
+  handleClockClick: function(whichClock) {
+    var newState = {};
+    newState[whichClock + 'Started'] = !this.state[whichClock + 'Started'];
+    this.setState(newState);
+    if (this.state.pause === true) {
+      alert('The jam is off, this timer will start onced you click the JAM START button.');
+    }
+  },
   handlePauseAll: function() {
     this.setState({pause: !this.state.pause});
   },
@@ -67,15 +66,16 @@ var Bout = React.createClass({
   changeColorText2: function(evt) {
     this.changeColor2(evt.target.value);
   },
-  switchJammers: function(){
-    var newOne, newFour;
-    newOne = 300000 - this.state.clockFour; 
+  switchJammers: function() {
+    var newOne;
+    var newFour;
+    newOne = 300000 - this.state.clockFour;
     newFour = 300000 - this.state.clockOne;
-    if (this.state.clockOne !== 300000 || this.state.clockFour !== 300000){
-        this.setState({
-          clockOne: newOne,
-          clockFour: newFour
-        });
+    if (this.state.clockOne !== 300000 || this.state.clockFour !== 300000) {
+      this.setState({
+        clockOne: newOne,
+        clockFour: newFour
+      });
     }
   },
   render: function() {
@@ -90,18 +90,18 @@ var Bout = React.createClass({
               <TextField hintText="Enter Color" floatingLabelText="Home Color:" style={{width: '95%'}} onChange={this.changeColorText} />
             </div>
             <div className="clock-button" style={{backgroundColor: this.state.color}}>
-                <OneClock handleReset={this.handleResetClick.bind(this, 'clockOne')} handleClick={this.handleClockClick.bind(this, 'clockOne')} 
-                started={this.state.clockOneStarted} maxTicks={30000} ticks={this.state.clockOne} callbackInterval={100} 
+                <OneClock handleReset={this.handleResetClick.bind(this, 'clockOne')} handleClick={this.handleClockClick.bind(this, 'clockOne')}
+                started={this.state.clockOneStarted} maxTicks={30000} ticks={this.state.clockOne} callbackInterval={100}
                 tickCallback={this.clockTickCallback.bind(this, 'clockOne')} />
             </div>
             <div className="clock-button" style={{backgroundColor: this.state.color}}>
-                <OneClock handleReset={this.handleResetClick.bind(this, 'clockTwo')} handleClick={this.handleClockClick.bind(this, 'clockTwo')} 
-                started={this.state.clockTwoStarted} maxTicks={30000} ticks={this.state.clockTwo} callbackInterval={100} 
+                <OneClock handleReset={this.handleResetClick.bind(this, 'clockTwo')} handleClick={this.handleClockClick.bind(this, 'clockTwo')}
+                started={this.state.clockTwoStarted} maxTicks={30000} ticks={this.state.clockTwo} callbackInterval={100}
                 tickCallback={this.clockTickCallback.bind(this, 'clockTwo')} />
             </div>
             <div className="clock-button" style={{backgroundColor: this.state.color}}>
-                <OneClock handleReset={this.handleResetClick.bind(this, 'clockThree')} handleClick={this.handleClockClick.bind(this, 'clockThree')} 
-                started={this.state.clockThreeStarted} maxTicks={30000} ticks={this.state.clockThree} callbackInterval={100} 
+                <OneClock handleReset={this.handleResetClick.bind(this, 'clockThree')} handleClick={this.handleClockClick.bind(this, 'clockThree')}
+                started={this.state.clockThreeStarted} maxTicks={30000} ticks={this.state.clockThree} callbackInterval={100}
                 tickCallback={this.clockTickCallback.bind(this, 'clockThree')} />
             </div>
             </div>
@@ -111,18 +111,18 @@ var Bout = React.createClass({
                 <TextField hintText="Enter Color" floatingLabelText="Visitor Color:" style={{width: '95%'}} onChange={this.changeColorText2} />
               </div>
               <div className="clock-button" style={{backgroundColor: this.state.color2}}>
-                <OneClock handleReset={this.handleResetClick.bind(this, 'clockFour')} handleClick={this.handleClockClick.bind(this, 'clockFour')} 
-                started={this.state.clockFourStarted} maxTicks={30000} ticks={this.state.clockFour} callbackInterval={100} 
+                <OneClock handleReset={this.handleResetClick.bind(this, 'clockFour')} handleClick={this.handleClockClick.bind(this, 'clockFour')}
+                started={this.state.clockFourStarted} maxTicks={30000} ticks={this.state.clockFour} callbackInterval={100}
                 tickCallback={this.clockTickCallback.bind(this, 'clockFour')} />
               </div>
               <div className="clock-button" style={{backgroundColor: this.state.color2}}>
-                <OneClock handleReset={this.handleResetClick.bind(this, 'clockFive')} handleClick={this.handleClockClick.bind(this, 'clockFive')} 
-                started={this.state.clockFiveStarted} maxTicks={30000} ticks={this.state.clockFive} callbackInterval={100} 
+                <OneClock handleReset={this.handleResetClick.bind(this, 'clockFive')} handleClick={this.handleClockClick.bind(this, 'clockFive')}
+                started={this.state.clockFiveStarted} maxTicks={30000} ticks={this.state.clockFive} callbackInterval={100}
                 tickCallback={this.clockTickCallback.bind(this, 'clockFive')} />
               </div>
             <div className="clock-button" style={{backgroundColor: this.state.color2}}>
-                <OneClock handleReset={this.handleResetClick.bind(this, 'clockSix')} handleClick={this.handleClockClick.bind(this, 'clockSix')} 
-                started={this.state.clockSixStarted} maxTicks={30000} ticks={this.state.clockSix} callbackInterval={100} 
+                <OneClock handleReset={this.handleResetClick.bind(this, 'clockSix')} handleClick={this.handleClockClick.bind(this, 'clockSix')}
+                started={this.state.clockSixStarted} maxTicks={30000} ticks={this.state.clockSix} callbackInterval={100}
                 tickCallback={this.clockTickCallback.bind(this, 'clockSix')} />
             </div>
             </div>
--- client/oneClock.js
+++ client/oneClock.js
@@ -9,9 +9,11 @@ var Clock = require('./clockText');
 var OneClock = React.createClass({
   propTypes: {
     pause: React.PropTypes.bool,
-    started: React.PropTypes.bool
+    started: React.PropTypes.bool,
+    ticks: React.PropTypes.number,
+    handleClick: React.PropTypes.func,
+    handleReset: React.PropTypes.func
   },
-
   getInitialState: function() {
     return {
       isPlaying: false,
@@ -19,42 +21,35 @@ var OneClock = React.createClass({
       maxtime: 30 * 10000,
     };
   },
-  initTimerInterval: function(props){
-    var that = this;
-    if(props.started && !this.timer){
-      this.timer = setInterval(props.tickCallback, props.callbackInterval || 1000);
-      console.log(props.started)
-    } else if (!props.started && this.timer || !props.started && props.ticks === 0) {
-      window.clearInterval(this.timer);
-      this.timer = null;
-      console.log(props.started)
-    }
-  },
   componentDidMount: function() {
-    this.initTimerInterval(this.props)
+    this.initTimerInterval(this.props);
   },
   componentWillReceiveProps: function(newProps) {
     if (this.state.time !== 300000) {
       if (this.props.pause === this.state.isPlaying) {
         this.handleStart();
       }
-    };
-    this.initTimerInterval(newProps)
+    }
+    this.initTimerInterval(newProps);
   },
   componentWillUnmount: function() {
     window.clearInterval(this.timer);
     this.timer = null;
   },
-  render: function() {
-    if (this.props.ticks > 0) {
-      var time = this.props.ticks;
-    } else {
-      var time = 0;
+  initTimerInterval: function(props) {
+    var that = this;
+    if (props.started && !this.timer) {
+      this.timer = setInterval(props.tickCallback, props.callbackInterval || 1000);
+    } else if (!props.started && this.timer || !props.started && props.ticks === 0) {
+      window.clearInterval(this.timer);
+      this.timer = null;
     }
+  },
+  render: function() {
     return (
       <div>
-        <button className="clock-float" onClick={this.props.handleClick} ><Clock time={time} /></button>
-        <button className="clock-float" onClick={this.props.handleClick}><i className='fa fa-5x fa-play'></i></button>
+        <button className="clock-float" onClick={this.props.handleClick} ><Clock time={this.props.ticks} /></button>
+        <button className="clock-float" onClick={this.props.handleClick}><i className="fa fa-5x fa-play"></i></button>
         <button className="reset" onClick={this.props.handleReset}> Reset </button>
       </div>
       );
